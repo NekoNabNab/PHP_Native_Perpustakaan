70354f1b69b718a3403d29a173b7fb90452e290b
Nabila Rozan Humairoh <101534076+NekoNabNab@users.noreply.github.com>
2023-05-12 11:34:16 +0700

    Update proses_tambah_buku.php

--- proses_tambah_buku.php
+++ proses_tambah_buku.php
@@ -16,7 +16,7 @@ if(!in_array($ext,$ekstensi) ) {
 	if($ukuran < 1044070){		
 		$xx =$filename;
 		move_uploaded_file($_FILES['foto']['tmp_name'], 'assets/foto_produk/'.''.$filename);
-		mysqli_query($conn, "INSERT INTO buku    VALUES(NULL,'$nama_buku','$pengarang','$xx', '$deskripsi')");
+		mysqli_query($conn, "INSERT INTO buku    VALUES(NULL,'$nama_buku','$deskripsi','$xx','$pengarang' )");
 		echo "<script>alert('Sukses menambahkan produk');location.href='tambah_buku.php';</script>";
 	}else{
 		echo "<script>alert('Gagal menambahkan produk');location.href='tambah_buku.php';</script>";
